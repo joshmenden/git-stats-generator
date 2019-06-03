34189b74eb528e1f105878d1bba2f1774f82d758
Josh Menden <jmenden@simplenexus.com>
2019-06-03 13:09:13 -0600

    Make line changes file a little more flexible

--- lineChanges.js
+++ lineChanges.js
@@ -1,7 +1,11 @@
+// Special thanks to https://gist.github.com/eyecatchup/3fb7ef0c0cbdb72412fc for the git command here
 const shell = require('shelljs')
 module.exports = function ({ author, since }) {
+
+  const linesAddedName = "Total Lines Added"
+  const linesDeletedName = "Total Lines Deleted"
   
-  buffer = shell.exec(`git log --author="${author}" --since="${since}" --pretty=tformat: --numstat | awk '{inserted+=$1; deleted+=$2; delta+=$1-$2;} END {printf "Total Lines Added::%s\\nTotal Lines Deleted::%s\\nTotal Lines Delta::%s", inserted, deleted, delta, ratio }'`, { silent: true }).stdout
+  buffer = shell.exec(`git log --author="${author}" --since="${since}" --pretty=tformat: --numstat | awk '{inserted+=$1; deleted+=$2;} END {printf "${linesAddedName}::%s\\n${linesDeletedName}::%s", inserted, deleted }'`, { silent: true }).stdout
 
   stats = {}
   lines = buffer.toString().split('\n')
@@ -9,5 +13,10 @@ module.exports = function ({ author, since }) {
     let [statName, result] = line.split("::")
     stats[statName] = Number(result)
   })
+
+  // Here you can add any combination of the above two stats
+  stats['Total Lines Delta'] = stats[linesAddedName] - stats[linesDeletedName]
+  stats['Lines Add:Delete (1: n) Ratio'] = stats[linesDeletedName] / stats[linesAddedName]
+
   return stats
 }
